c528995b1c8127da7e51d02cc644ffa01694deed
Evan Stark <estark3@charlotte.edu>
2025-05-05 21:10:04 -0400

    Updated script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,3 @@
-# Evan Stark - May 2nd 2025 - ITCS 4145 001
-# SLURM Script to allocate a GPU node and to run NBody-CUDA implementation.
-
 #!/bin/bash
 #SBATCH --job-name=nbody_cuda
 #SBATCH --partition=GPU
@@ -10,7 +7,6 @@
 #SBATCH --gres=gpu:1
 
 module load cuda/12.4
-
 salloc --partition=GPU --time=01:00:00 --gres=gpu:1
 nvcc -o nbody_cuda nbody_cuda.cu planet 200 5000000 10000
 srun --pty bash
